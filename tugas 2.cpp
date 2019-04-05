5a91900fd793f791663326d394de799e699bd057
andreadi7 <49295507+andreadi7@users.noreply.github.com>
2019-04-05 09:31:56 +0700

    TUGAS PEMLAN
    TUGAS IN & OVER

---
+++ tugas 2.cpp
@@ -0,0 +1,62 @@
+// nama : ANDRE ADIKUSUMA
+// NIM  : 185150300111049
+// KELAS : TEKKOM - D
+
+#include<iostream>
+using namespace std;
+
+class  apel1{
+	public :
+	int x;
+	void nilaix(){
+		cout<<"jumlah apel milik X = "; cin>>x;
+	}
+	void overriding(){
+		cout<<"di beli"<<endl;
+	}
+};
+
+class apel2{
+	public :
+	int y;
+	void nilaiy(){
+		cout<<"jumlah apel milik Y = ";cin>>y;
+	}
+	void overriding(){
+		cout<<"di cari"<<endl;
+	}
+};
+
+class hitung : public apel1,public apel2{
+	public :
+	void hit(){
+		cout<<"jumlah seluruh apel = "<<x+y<<endl;
+	}
+	void overriding(){
+		cout<<"di makan"<<endl;
+	}
+		
+};
+
+int main(){
+	hitung has1;
+	has1.nilaix();
+	has1.nilaiy();
+	has1.hit();
+	cout<<endl;
+	
+	apel1 beli;
+	cout<<"Apel X ";
+	beli.overriding();
+	cout<<endl;
+	
+	apel2 jual;
+	cout<<"Apel Y ";
+	jual.overriding();
+	cout<<endl;
+	
+	hitung makan;
+	cout<<"Semua Apel ";
+	makan.overriding();
+	cout<<endl;
+}
